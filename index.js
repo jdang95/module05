c0c3e03ca97e8ca88860c747791cbc6e77203766
Julia Dang <43449736+jdang95@users.noreply.github.com>
2018-10-25 09:56:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,28 @@
+ var num = prompt("Please enter Number from 0-10", "") //prompt user to enter the number
+ 
 
-body {
-	background-color: #523620;
-	color: #fff;
-	font-family: 'Wellfleet', times, serif;
-	font-size: 150%;
-	margin: 0px 0px 0px 0px;}
+var table = num;             // Unit of table
+var operator = 'multiple'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';             // Message
 
-h1, h3{
-    color: white;
-    text-align: center;
-    font-family: 'ComputerSpeakv03Regular'; 
-  
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
 }
 
-	
-
-#teacher {
-	float:right;
-	margin:0px 30px 0px 0px;}
-#teacher2 {
-	float:right;
-	margin:135px 30px 0px 0px;}
-
-#page1, #page2 {
-	background-color: #fecc6f;
-	height: 596px;
-	padding: 10px;
-	min-width: 779px;}
-#page2 {
-	height: 730px;}
-
-#answer {
-	border: 12px double #fff;
-	color: #523620;
-	text-align: left;
-	padding: 20px;
-	margin: 70px auto 10px auto;
-	width: 250px;
-	text-align: center;}
-
-#blackboard {
-	background-color: #425a5a;
-	border: 25px solid #523620;
-	border-radius: 20px;
-	padding: 40px 20px;
-	margin: 0px auto;
-	height: 350px;
-	width: 700px;
-	text-align: center;}
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
 
 
 
-   
-}
